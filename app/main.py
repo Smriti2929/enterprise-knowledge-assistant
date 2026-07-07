2d044353798bda6cb0f99280f17a2411066ee8c4
Smriti Asthana <smrtsthn@gmail.com>
2026-07-08 00:28:18 +0530

    Add SQLAlchemy ORM user model and database table

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from sqlalchemy import text
 
-from app.core.database import engine
+from app.core.database import Base, engine
+
+from app.models import User
 
 app = FastAPI(title= "Enterprise Knowledge Assistant")
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
@@ -11,6 +15,7 @@ def root():
 
 @app.get("/health")
 def health_check():
+    #The with statement automatically closes the connection afterward.
     with engine.connect() as connection:
         connection.execute(text("SELECT 1"))
     return {"status": "Databse Connected Successfully"}    
---
+++ app/models/user.py
@@ -0,0 +1,20 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.core.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100))
+
+    email:Mapped[str] = mapped_column(
+        String(255),
+        unique=True,
+        nullable=False
+    )
+
+    hashed_password: Mapped[str] = mapped_column(
+        String(255)
+    )
